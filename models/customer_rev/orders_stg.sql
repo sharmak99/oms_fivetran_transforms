bd61d4ed6f823d72a5e3d6e7fc9bbfdf4a1caa17
Kuldeep Sharma <209468840+sharmak99@users.noreply.github.com>
2026-04-17 19:17:09 +0530

    Update orders_stg.sql

--- models/customer_rev/orders_stg.sql
+++ models/customer_rev/orders_stg.sql
@@ -3,8 +3,8 @@ SELECT
     name,    
     age,
     city,
-    salary,
-    CASE
+    salary
+    , CASE
         WHEN salary >= 100000 THEN 'High'
         WHEN salary >= 50000 THEN 'Medium'
         ELSE 'Low'
